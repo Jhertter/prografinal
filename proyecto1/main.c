08b61bd464fa51158b6b9fe3090472fe8c1b0625
ivan <jhertter@itba.edu.ar>
2022-02-09 12:24:16 -0300

    proyecto v0.0

---
+++ proyecto1/main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: ivan
+ *
+ * Created on 9 de febrero de 2022, 12:18
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) 
+{
+    printf("holis\n");
+    return (EXIT_SUCCESS);
+}
+
